9f6256e9caafa396657d7d54fa3b863f9202e538
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2022-11-04 06:19:33 +0100

    Fixed icons velocity page and added test form

--- assets/scripts/include_content.js
+++ assets/scripts/include_content.js
@@ -26,3 +26,27 @@ function includeHTML(fileName, destination) {
 includeHTML("header.html", document.querySelector(".header"));
 includeHTML("footer.html", document.querySelector(".footer"));
 
+// Popovers (Bootstrap)
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+// Allow popover entries to accept HTML mark-up.
+$(function() {
+  $('[data-toggle="popover"]').popover({
+    html: true
+  });
+  $('[title="popover"]').popover({
+    html: true
+  });
+});
+
+// disable mousewheel on an input number field when in focus
+// (Prevents an accidental change in input values when scrolling on the page)
+$('form').on('focus', 'input[type=number]', function(e) {
+  $(this).on('wheel.disableScroll', function(e) {
+    e.preventDefault();
+  });
+});
+$('form').on('blur', 'input[type=number]', function(e) {
+  $(this).off('wheel.disableScroll');
+})

3856868452dff0feb3088cf7da96cbe04ef54070
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2022-11-04 18:53:22 +0100

    Add conversion for distances

--- assets/scripts/calculators/velocity.js
+++ assets/scripts/calculators/velocity.js
@@ -38,3 +38,40 @@ function convert() {
   document.getElementById("fts").innerHTML =
     (result * 3.28084).toFixed(2) + "<span> fts<sup>-1</sup></span>";
 }
+
+//convert distances into m regardless of choice of unit
+dElement = document.getElementById("inputDistance");
+dSecondElement = document.getElementById("unitsDistance");
+dElement.addEventListener("keyup", dConvert);
+dSecondElement.addEventListener("click", dConvert);
+function dConvert() {
+  let di = parseFloat(dElement.value);
+  let dUnit = document.getElementById("unitsDistance").value;
+  let dResult;
+  if (dUnit === "m") {
+    dResult = di;
+  }
+  if (dUnit === "km") {
+    dResult = di * 1000;
+  }
+  if (dUnit === "yd") {
+    dResult = di * 0.44704;
+  }
+  if (dUnit === "mi") {
+    dResult = di / 0.000621371;
+  }
+   if (dUnit === "ft") {
+     dResult = di / 3.28084;
+   }
+  if (isNaN(dResult)) dResult = 0;
+  document.getElementById("m").innerHTML =
+    dResult.toFixed(2) + "<span> m<span>";
+  document.getElementById("km").innerHTML =
+    (dResult / 1000).toFixed(2) + "<span> km</span>";
+  document.getElementById("yd").innerHTML =
+    (dResult * 1.09361).toFixed(2) + "<span> yd</span>";
+  document.getElementById("mi").innerHTML =
+    (dResult * 0.000621371).toFixed(2) + "<span> mi</span>";
+  document.getElementById("ft").innerHTML =
+    (dResult * 3.28084).toFixed(2) + "<span> ft</span>";
+}

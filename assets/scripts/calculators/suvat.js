ad5198784178e59cd3999efea0344c5a683b2613
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2022-11-06 17:41:56 +0100

    Checkbox and inputs max three allowed: validated

---
+++ assets/scripts/calculators/suvat.js
@@ -0,0 +1,29 @@
+//disable checkboxes when a max 4 have been selected
+var $checkboxes = $(".suvat-check input[type=checkbox]");
+$checkboxes.on("change", function () {
+  if ($checkboxes.filter(":checked").length >= 4) {
+    $checkboxes.filter(":not(:checked)").prop("disabled", true);
+  } else {
+    $checkboxes.prop("disabled", false);
+  }
+});
+
+//enable inputs based on checked box )(up to 4 possible)
+document.getElementById("displacement").onchange = function () {
+document.getElementById("inputDistance").disabled = !this.checked;
+};
+document.getElementById("initial_velocity").onchange = function () {
+document.getElementById("inputVi").disabled = !this.checked;
+};
+document.getElementById("final_velocity").onchange = function () {
+document.getElementById("inputVf").disabled = !this.checked;
+};
+document.getElementById("acceleration").onchange = function () {
+document.getElementById("inputAcceleration").disabled = !this.checked;
+};
+document.getElementById("time").onchange = function () {
+document.getElementById("inputTime").disabled = !this.checked;
+};
+
+
+

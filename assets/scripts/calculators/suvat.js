cb6f4518fbf9e9d2d095272941d15bc339585022
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2022-11-06 20:33:10 +0100

    Prevent sub when 1 or 2 checkboxes enabled

--- assets/scripts/calculators/suvat.js
+++ assets/scripts/calculators/suvat.js
@@ -1,14 +1,20 @@
 //disable checkboxes when a max 4 have been selected
 var $checkboxes = $(".suvat-check input[type=checkbox]");
+var $submitButton = $("#submitButton");
 $checkboxes.on("change", function () {
   if ($checkboxes.filter(":checked").length >= 4) {
     $checkboxes.filter(":not(:checked)").prop("disabled", true);
   } else {
     $checkboxes.prop("disabled", false);
-  }
+    }
+    if ($checkboxes.filter(":checked").length <= 2) {
+        $submitButton.prop("disabled", true);
+    } else {
+        $submitButton.prop("disabled", false);
+}
 });
 
-//enable inputs based on checked box )(up to 4 possible)
+//enable inputs based on checked box (up to 4 possible)
 document.getElementById("displacement").onchange = function () {
 document.getElementById("inputDistance").disabled = !this.checked;
 };

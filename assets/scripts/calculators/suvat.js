8a6b772fff8647905754fd3b8538ae77f1e2751f
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2022-11-08 20:51:42 +0100

    Disable gravity option when acceleration unchecked

--- assets/scripts/calculators/suvat.js
+++ assets/scripts/calculators/suvat.js
@@ -1,6 +1,7 @@
 //disable checkboxes when a max 4 have been selected
 var $checkboxes = $(".suvat-check input[type=checkbox]");
 var $submitButton = $("#submitButton");
+var $accelerationSelect = $("#acceleration").prop("checked");
 $checkboxes.on("change", function () {
   if ($checkboxes.filter(":checked").length >= 4) {
     $checkboxes.filter(":not(:checked)").prop("disabled", true);
@@ -11,7 +12,11 @@ $checkboxes.on("change", function () {
         $submitButton.prop("disabled", true);
     } else {
         $submitButton.prop("disabled", false);
-    }
+  }
+
+  if ($accelerationSelect.prop(":checked")) {
+    $("#aUnits").prop("disabled", false);
+  }
 });
 
 //enable inputs based on checked box (up to 4 possible)
@@ -30,6 +35,27 @@ document.getElementById("inputAcceleration").disabled = !this.checked;
 document.getElementById("time").onchange = function () {
 document.getElementById("inputTime").disabled = !this.checked;
 };
+
+//Enable or disable selection of gravity based on user variable selection.
+document.querySelector("#acceleration").addEventListener("change", gravSelect);
+function gravSelect() {
+  if (document.getElementById("acceleration").checked) {
+    document.getElementById("gOnly").disabled = false;
+  }
+}
+
+//If Acceleration is chosen and gravity is the chosen acceleration update the input
+//to reflect this.
+const unitsSelect = document.getElementById("aUnits"),
+gravityInput = document.getElementById("inputAcceleration");
+unitsSelect.addEventListener("change", (evt) => {
+  const unitSelected = unitsSelect.value;
+  if (unitSelected === "g") {
+    gravityInput.value = "9.81";
+  } else {
+    gravityInput.value = "";
+  }
+});
 
 
 

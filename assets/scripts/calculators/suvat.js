f2a96b19112253917082f00adcd515e7566e34ed
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2022-11-09 22:05:19 +0100

    Added 15 eqns with temp output in jumbotron

--- assets/scripts/calculators/suvat.js
+++ assets/scripts/calculators/suvat.js
@@ -57,5 +57,195 @@ unitsSelect.addEventListener("change", (evt) => {
   }
 });
 
-
-
+//Equation Variables
+//Checkbox selection
+const s1 = document.querySelector("#displacement");
+const u1 = document.querySelector("#initial_velocity");
+const v1 = document.querySelector("#final_velocity");
+const a1 = document.querySelector("#acceleration");
+const t1 = document.querySelector("#time");
+//Determine the equation and evaluate
+//s.checked === false, et al. can be reduced to !s.checked and
+//u.checked === true can be reduced to u.checked
+document.getElementById("submitButton").addEventListener("click", calculateMe);
+function calculateMe() {
+  //User input values
+  const s = parseFloat(document.getElementById("inputDistance").value);
+  const u = parseFloat(document.getElementById("inputVi").value);
+  const v = parseFloat(document.getElementById("inputVf").value);
+  const a = parseFloat(document.getElementById("inputAcceleration").value);
+  const t = parseFloat(document.getElementById("inputTime").value);
+  let result;
+  let result2;
+  if (
+    !s1.checked &&
+    u1.checked &&
+    v1.checked &&
+    a1.checked &&
+    t1.checked
+  ) {
+    //Equation 1
+    result = u * t + 0.5 * a * (Math.pow(t,2));
+    document.getElementById("trial").innerHTML = result;
+  } else if (
+    !s1.checked &&
+    !u1.checked &&
+    v1.checked &&
+    a1.checked &&
+    t1.checked
+  ) {
+    //equation 2
+    result = v - a * t;
+    result2 = result * t + 0.5 * a * (Math.pow(t,2));
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    !s1.checked &&
+    u1.checked &&
+    !v1.checked &&
+    a1.checked &&
+    t1.checked
+  ) {
+    //equation 3
+    result = u + a * t;
+    result2 = u * t + 0.5 * a * Math.pow(t, 2);
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    !s1.checked &&
+    u1.checked &&
+    v1.checked &&
+    !a1.checked &&
+    t1.checked
+  ) {
+    //equation 4
+    result = (v - u) / t;
+    result2 = u * t + 0.5 * result * Math.pow(t, 2);
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    !s1.checked &&
+    u1.checked &&
+    v1.checked &&
+    a1.checked &&
+    !t1.checked
+  ) {
+    //equation 5
+    result = (v - u) / a;
+    result2 = u * result + 0.5 * a * Math.pow(result, 2);
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    s1.checked &&
+    !u1.checked &&
+    v1.checked &&
+    a1.checked &&
+    t1.checked
+  ) {
+    //equation 6
+    result = v - a * t;
+    document.getElementById("trial").innerHTML = result;
+  } else if (
+    s1.checked &&
+    !u1.checked &&
+    !v1.checked &&
+    a1.checked &&
+    t1.checked
+  ) {
+    //equation 7
+    result = s / t - a * t / 2;
+    result2 = result + a * t;
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    s1.checked &&
+    !u1.checked &&
+    v1.checked &&
+    !a1.checked &&
+    t1.checked
+  ) {
+    //equation 8
+    result = (2 * s / t) - v;
+    result2 = (v - result) / t;
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    s1.checked &&
+    !u1.checked &&
+    v1.checked &&
+    a1.checked &&
+    !t1.checked
+  ) {
+    //equation 9
+    result = Math.sqrt(Math.pow(v, 2) - 2 * a * s);
+    result2 = (v - result) / a;
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    s1.checked &&
+    u1.checked &&
+    !v1.checked &&
+    a1.checked &&
+    t1.checked
+  ) {
+    //equation 10
+    result = u + a * t;
+    document.getElementById("trial").innerHTML = result;
+  } else if (
+    s1.checked &&
+    u1.checked &&
+    !v1.checked &&
+    !a1.checked &&
+    t1.checked
+  ) {
+    //equation 11
+    result = (2 * s / t) - u;
+    result2 = (result - u) / t;
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    s1.checked &&
+    u1.checked &&
+    !v1.checked &&
+    a1.checked &&
+    !t1.checked
+  ) {
+    //equation 12
+    result = Math.sqrt(Math.pow(u, 2) + 2 * a * s);
+    result2 = (result - u) / a;
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    s1.checked &&
+    u1.checked &&
+    v1.checked &&
+    !a1.checked &&
+    t1.checked
+  ) {
+    //equation 13
+    result = (v - u) / t;
+    document.getElementById("trial").innerHTML = result;
+  } else if (
+    s1.checked &&
+    u1.checked &&
+    v1.checked &&
+    !a1.checked &&
+    !t1.checked
+  ) {
+    //equation 14
+    result = (v - u) * (v + u) / (2 * s);
+    result2 = (v - u) / result;
+    document.getElementById("trial").innerHTML = result;
+    document.getElementById("trial2").innerHTML = result2;
+  } else if (
+    s1.checked &&
+    u1.checked &&
+    v1.checked &&
+    a1.checked &&
+    !t1.checked
+  ) {
+    //equation 15
+    result = (v - u) / a;
+    document.getElementById("trial").innerHTML = result;
+  }
+}
